128ceb653f18064e1e5be52998ca94a1f9894831
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-04-30 16:22:48 +0300

    Added DocumentTypeHierarchy Entity

---
+++ src/DocumentProject/Domain/Document/DocumentTypeHierarchy.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Document
+{
+    public class DocumentTypeHierarchy : Entity<int>
+    {
+        public int MacroId { get; set; }
+        public DocumentType Macro { get; set; }
+        public int MicroId { get; set; }
+        public DocumentType Micro { get; set; }
+    }
+}
