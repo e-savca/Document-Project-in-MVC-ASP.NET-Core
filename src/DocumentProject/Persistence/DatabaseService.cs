d3c5a3a0336b8466cdc4f5d19e6687a28ef19300
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-04-30 17:36:06 +0300

    Added SeedDataRepo and seed data in json file

---
+++ src/DocumentProject/Persistence/DatabaseService.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces;
+using Domain.Document;
+using Domain.Institute;
+using Domain.Project;
+using Domain.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Persistence
+{
+    public class DatabaseService : DbContext, IDatabaseService
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Role> Roles { get; set; } = null!;
+        public DbSet<UserRole> UserRoles { get; set; } = null!;
+        public DbSet<Document> Documents { get; set; } = null!;
+        public DbSet<DocumentType> DocumentTypes { get; set; } = null!;
+        public DbSet<Institute> Institutes { get; set; } = null!;
+        public DbSet<Project> Projects { get; set; } = null!;
+
+        public DatabaseService(DbContextOptions<DatabaseService> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public void Save()
+        {
+            this.SaveChanges();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            return this.SaveChangesAsync();
+        }
+
+        public Task<int> SaveAsync(CancellationToken cancellationToken)
+        {
+            return this.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/DocumentProject/Persistence/SeedDataRepo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Document;
+using Domain.Institute;
+using Domain.Project;
+using Domain.User;
+using Newtonsoft.Json;
+
+namespace Persistence
+{
+    public class SeedDataRepo
+    {
+        private readonly string jsonFilePath = @"/App_Data/test.json";
+        private readonly List<User> _users;
+        private readonly List<Role> _roles;
+        private readonly List<UserRole> _userRoles;
+        private readonly List<Institute> _institutes;
+        private readonly List<Project> _projects;
+        private readonly List<DocumentType> _documentTypes;
+        private readonly List<DocumentTypeHierarchy> _documentTypeHierarchies;
+        private readonly List<Document> _documents;
+
+        public SeedDataRepo(string jsonFilePath)
+        {
+            // get current directory path from where the program is running
+
+            var jsonData = File.ReadAllText(jsonFilePath);
+            var data = JsonConvert.DeserializeObject(jsonData);
+            //_users = data["Users"];
+            //_roles = data.Roles;
+            //_userRoles = data.UserRoles;
+            //_institutes = data.Institutes;
+            //_projects = data.Projects;
+            //_documentTypes = data.DocumentTypes;
+            //_documentTypeHierarchies = data.DocumentTypeHierarchies;
+            //_documents = data.Documents;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _users;
+        }
+
+        public IEnumerable<Role> GetAllRoles()
+        {
+            return _roles;
+        }
+
+        public IEnumerable<UserRole> GetAllUserRoles()
+        {
+            return _userRoles;
+        }
+
+        public IEnumerable<Institute> GetAllInstitutes()
+        {
+            return _institutes;
+        }
+
+        public IEnumerable<Project> GetAllProjects()
+        {
+            return _projects;
+        }
+
+        public IEnumerable<DocumentType> GetAllDocumentTypes()
+        {
+            return _documentTypes;
+        }
+
+        public IEnumerable<DocumentTypeHierarchy> GetAllDocumentTypeHierarchies()
+        {
+            return _documentTypeHierarchies;
+        }
+
+        public IEnumerable<Document> GetAllDocuments()
+        {
+            return _documents;
+        }
+    }
+}
